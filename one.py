fe4940bff8556b5297b6dc531d7618f2ede12453
jaiminujariya <jaiminujariya1218@gmail.com>
2026-01-03 11:26:22 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,29 @@
+"""print("hellow")"""
+"""x = 20
+print (x)"""
+"""x = 20.5
+print (x)"""
+"""x = 1j
+print (x)"""
+"""x = True or False
+print (x)"""
+"""x = None 
+print (x)"""
+"""x = ["apple","banana","mango"]
+print (x)"""
+"""x = ("atmiya","marvadi","darshan")
+print (x)"""
+"""x = range(6)
+print (x)"""
+"""x = {"name":"john","age":"36"}
+print (x)"""
+"""x = {"apple","banana","cherry"}
+print (x)"""
+"""x = frozenset ({"apple","banana","cherry"})
+print (x)"""
+"""x = b "hellowww"
+print (x)"""
+"""x = bytearray(s)
+print (x)"""
+x = memoryview (bytes(s))
+print (x)
