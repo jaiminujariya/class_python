af4dff9d0f95cbee44bf498364c7cfbde883bfcd
jaiminujariya <jaiminujariya1218@gmail.com>
2026-01-31 10:03:38 +0530

    Add files via upload

---
+++ oparetor.py
@@ -0,0 +1,48 @@
+#(1)
+#a=10
+#b=10
+#print(a+b)
+
+#(2)
+#a=5
+#print(-a)
+
+#(3)
+#p=10
+#q=5
+#print(p>q)
+#print(p==q)
+
+#(4)
+#x=True
+#y=False
+#print(x and y)
+#print(x or y)
+#print(not y)
+
+#(5)
+#is_jaimin=True
+#print(is_jaimin)
+
+#(6)
+#a=5
+#b=3
+#print(a&b)
+#print(a|b)
+
+#(7)
+#numbers=[11,12,13,14,15]
+#print(12 in numbers)
+#print(10 not in numbers)
+
+#(8)
+#a=10
+#b=10
+#print(a is b)
+#print(a is not y)
+
+#(9)
+a=10
+b=3
+print(a+b)
+print(a%b)
